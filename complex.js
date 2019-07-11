423aaea3484400d6cc2ff63fd18e8d5ed11e5367
davidforrest <davidmforrest@gmail.com>
2019-07-11 13:20:30 -0400

    a working Complex scraper

--- complex.js
+++ complex.js
@@ -1,34 +1,21 @@
-// TODO: trim & separate artist and song
-// TODO: separate out the video ID
-// TODO: add source into the song record. make it a complete song record.
+// TODO: format the date and time (using moment.js?)
 // TODO: push these into a JSON
-// TODO: add dateAdded & format dates (using moment?)
+// TODO: scroll to the bottom of the page first, so all the React stuff loads.
 
-// sourceDate
-element = document.getElementsByClassName("info-row__datetime");
-console.log(element[0].innerHTML);
-
-// artist(s) + title
-elements = document.getElementsByClassName("article__copy clearfix");
-element = elements[0].getElementsByTagName('h2');
-for (var i=0; i<element.length; i++){
-  console.log("artist+song: " + element[i].innerHTML);
-};
-
-
-// VID
-vid = document.getElementsByClassName("video-lazyload");
-for (var i=0; i<vid.length; i++){
-  console.log(vid[i].style.backgroundImage);
-};
-
-// everything
 elements = document.getElementsByClassName("article__copy clearfix");
 element = elements[0].getElementsByTagName('h2');
 sourceDate = document.getElementsByClassName("info-row__datetime");
-vid = document.getElementsByClassName("video-lazyload");
+vidUrl = document.getElementsByClassName("video-lazyload");
 for (var i=0; i<element.length; i++){
-  setTimeout (console.log.bind (console, "artist+song: " + element[i].innerHTML.trim()));
+  merged = element[i].innerHTML;
+  song = merged.match(/, “(.*?)”/)[1];
+  artist = merged.match(/.+?(?=, “)/);
+  vid = vidUrl[i].style.backgroundImage.match(/vi\/([^\/]{0,})/)[1];
+
+  setTimeout (console.log.bind (console, "dateAdded: " + new Date()));
+  setTimeout (console.log.bind (console, "source: " + "Complex Best New Music This Week"));
   setTimeout (console.log.bind (console, "sourceDate: " + sourceDate[0].innerHTML.trim()));
-  setTimeout (console.log.bind (console, "vid: " + vid[i].style.backgroundImage.trim()));
+  setTimeout (console.log.bind (console, "songName: " + song));
+  setTimeout (console.log.bind (console, "artistName: " + artist));
+  setTimeout (console.log.bind (console, "videoID: " + vid));
 };
