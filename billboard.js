06d6308f13c2ada252cf0aa453a428c6f589fa9f
davidforrest <davidmforrest@gmail.com>
2019-07-16 09:09:28 -0400

    updated billboard scraper and added sourceUrl

--- billboard.js
+++ billboard.js
@@ -1,3 +1,4 @@
+sourceDate = document.getElementsByClassName('chart-detail-header__date-selector-button');
 elements = document.getElementsByClassName('chart-list-item');
 for (var i=0; i<elements.length; i++){
     var isNew = false;
@@ -9,16 +10,13 @@ for (var i=0; i<elements.length; i++){
     var title = element.getElementsByClassName('chart-list-item__title')[0];
     var artist = element.getElementsByClassName('chart-list-item__artist')[0];
     if(isNew){
+
+    setTimeout (console.log.bind (console, "dateAdded: " + new Date()));
+    setTimeout (console.log.bind (console, "source: " + "Billboard Hot 100"));
+    setTimeout (console.log.bind (console, "sourceDate: " + sourceDate[0].innerText));
+    setTimeout (console.log.bind (console, "sourceUrl: " + window.location.href));
     setTimeout (console.log.bind (console, "songName: " + title.textContent.trim()));
     setTimeout (console.log.bind (console, "artistName: " + artist.textContent.trim()));
-    setTimeout (console.log.bind (console, "sourceDate: 19.7.13"));
+    setTimeout (console.log.bind (console, "videoID: " + ""));
   }
-}
-
-// the source date
-sourceDate = document.getElementsByClassName('chart-detail-header__date-selector-button');
-console.log(sourceDate[0].innerText)
-
-//the date added
-dateAdded = new Date();
-console.log()
+};
--- complex.js
+++ complex.js
@@ -1,6 +1,7 @@
 // TODO: format the date and time (using moment.js?)
 // TODO: push these into a JSON
-// TODO: scroll to the bottom of the page first, so all the React stuff loads.
+// Scroll to the bottom of the page first, so all the React stuff loads.
+// Always check the last song name to make sure everything got scraped.
 
 elements = document.getElementsByClassName("article__copy clearfix");
 element = elements[0].getElementsByTagName('h2');
@@ -8,13 +9,14 @@ sourceDate = document.getElementsByClassName("info-row__datetime");
 vidUrl = document.getElementsByClassName("video-lazyload");
 for (var i=0; i<element.length; i++){
   merged = element[i].innerHTML;
-  song = merged.match(/, “(.*?)”/)[1];
-  artist = merged.match(/.+?(?=, “)/);
+  song = merged.match(/, “(.*?)”/)[1]; // may need " type quotation marks
+  artist = merged.match(/.+?(?=, “)/); // may need " type quotation marks
   vid = vidUrl[i].style.backgroundImage.match(/vi\/([^\/]{0,})/)[1];
 
   setTimeout (console.log.bind (console, "dateAdded: " + new Date()));
   setTimeout (console.log.bind (console, "source: " + "Complex Best New Music This Week"));
   setTimeout (console.log.bind (console, "sourceDate: " + sourceDate[0].innerHTML.trim()));
+  setTimeout (console.log.bind (console, "sourceUrl: " + window.location.href));
   setTimeout (console.log.bind (console, "songName: " + song));
   setTimeout (console.log.bind (console, "artistName: " + artist));
   setTimeout (console.log.bind (console, "videoID: " + vid));
