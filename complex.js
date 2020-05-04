8ef6d99e338c34a771d4c38b13dc03763e279141
davidforrest <davidmforrest@gmail.com>
2020-05-03 22:37:44 -0700

    Output JSON instead of console.log

--- complex.js
+++ complex.js
@@ -1,5 +1,4 @@
 // TODO: format the date and time (using moment.js?)
-// TODO: push these into a JSON
 // Scroll to the bottom of the page first, so all the React stuff loads.
 // Always check the last song name to make sure everything got scraped.
 
@@ -7,17 +6,35 @@ elements = document.getElementsByClassName("article__copy clearfix");
 element = elements[0].getElementsByTagName("h2");
 sourceDate = document.getElementsByClassName("info-row__datetime");
 vidUrl = document.getElementsByClassName("custom-embed")
+
+array = [];
+
 for (var i=0; i<element.length; i++){
   merged = element[i].innerText;
   song = merged.match(/, “(.*?)”/)[1]; // may need " type quotation marks, or a comma after the artist name
   artist = merged.match(/.+?(?=, “)/)[0]; // may need " type quotation marks
   vid = vidUrl[i].getElementsByTagName("iframe")[0].src.match(/embed\/([^"]{0,})/)[1];
 
-  console.log("dateAdded: " + new Date());
-  console.log("source: " + "Complex Best New Music This Week");
-  console.log("sourceDate: " + sourceDate[0].innerHTML.trim());
-  console.log("sourceUrl: " + window.location.href);
-  console.log("songName: " + song);
-  console.log("artistName: " + artist);
-  console.log("videoID: " + vid);
+  obj = {
+    "dateAdded": "",
+    "source": "",
+    "sourceDate": "",
+    "sourceUrl": "",
+    "songName": "",
+    "artistName": "",
+    "videoID": ""
+  };
+
+  obj.dateAdded = new Date();
+  obj.source = "Complex Best New Music This Week";
+  obj.sourceDate = sourceDate[0].innerHTML.trim();
+  obj.sourceUrl = window.location.href;
+  obj.songName = song;
+  obj.artistName = artist;
+  obj.videoID = vid;
+
+  array.push(obj);
+
 };
+
+JSON.stringify(array, null, 4)
