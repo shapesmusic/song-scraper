16a06ae4e7e8a76fe52af93715ecbad0c4554a30
davidforrest <davidmforrest@gmail.com>
2020-05-04 20:37:47 -0700

    Add moment and format dates

--- complex.js
+++ complex.js
@@ -1,48 +1,59 @@
-// TODO: format the date and time (using moment.js?)
 // Scroll to the bottom of the page first, so all the React stuff loads.
 // Always check the last song name to make sure everything got scraped.
 
-// get source data
-chartTitle = document.getElementsByClassName("story-title story-title__article")[0].innerText;
-parentStream = chartTitle.match(/.+?(?=:)/)[0];
-instanceName = chartTitle.match(/[^:]+$/)[0].trim();
+//
+// Step 1: get source data
+//
+  chartTitle = document.getElementsByClassName("story-title story-title__article")[0].innerText;
+  parentStream = chartTitle.match(/.+?(?=:)/)[0];
+  instanceName = chartTitle.match(/[^:]+$/)[0].trim();
 
-publicationDate = document.getElementsByClassName("info-row__datetime");
+  // add moment.js to the header
+  momentjs = document.createElement("script");
+  momentjs.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.2.1/moment.min.js";
+  document.head.appendChild(momentjs);
 
-source = { // a streamInstance
-  "parentEntity": "Complex",
-  "parentStream": parentStream,
-  "instanceName": instanceName,
-  "publicationDate": publicationDate[0].innerHTML.trim(), // previously "sourceDate"
-  "location": window.location.href, // previously "sourceUrl"
-};
+  // get and format publicationDate
+  publicationDate = document.getElementsByClassName("info-row__datetime")[0].innerHTML.trim();
+  publicationDateFormatted = moment(publicationDate, "MMM DD, YYYY").format(); // to ISO
 
-JSON.stringify(source, null, 4)
-
-
-// get songs data
-elements = document.getElementsByClassName("article__copy clearfix");
-element = elements[0].getElementsByTagName("h2");
-videoUrl = document.getElementsByClassName("custom-embed")
-
-songs = [];
-
-for (var i=0; i<element.length; i++){
-  merged = element[i].innerText;
-  songName = merged.match(/, “(.*?)”/)[1]; // may need " type quotation marks, or a comma after the artist name
-  artistName = merged.match(/.+?(?=, “)/)[0]; // may need " type quotation marks
-  videoId = videoUrl[i].getElementsByTagName("iframe")[0].src.match(/embed\/([^"]{0,})/)[1];
-
-  song = {
-    "captureDate": new Date(), // previously "dateAdded" (not used by the player)
-    "captureSource": "", // TODO: a reference to source
-    "songName": songName,
-    "artistName": artistName,
-    "videoId": videoId
+  // build the object
+  source = { // a streamInstance
+    "parentEntity": "Complex",
+    "parentStream": parentStream,
+    "instanceName": instanceName,
+    "publicationDate": publicationDateFormatted, // previously "sourceDate"
+    "location": window.location.href, // previously "sourceUrl"
   };
 
-  songs.push(song);
+  JSON.stringify(source, null, 4)
 
-};
 
-JSON.stringify(songs, null, 4)
+//
+// Step 2: get songs data
+//
+  elements = document.getElementsByClassName("article__copy clearfix");
+  element = elements[0].getElementsByTagName("h2");
+  videoUrl = document.getElementsByClassName("custom-embed")
+
+  songs = [];
+
+  for (var i=0; i<element.length; i++){
+    merged = element[i].innerText;
+    songName = merged.match(/, “(.*?)”/)[1]; // may need " type quotation marks, or a comma after the artist name
+    artistName = merged.match(/.+?(?=, “)/)[0]; // may need " type quotation marks
+    videoId = videoUrl[i].getElementsByTagName("iframe")[0].src.match(/embed\/([^"]{0,})/)[1];
+
+    song = {
+      "captureDate": moment(new Date()).format(), // previously "dateAdded" (not used by the player)
+      "captureSource": "", // TODO: a reference to source
+      "songName": songName,
+      "artistName": artistName,
+      "videoId": videoId
+    };
+
+    songs.push(song);
+
+  };
+
+  JSON.stringify(songs, null, 4)
