ff2d59aad348ee8f8a96aae586e7af1598285027
davidforrest <davidmforrest@gmail.com>
2020-05-12 10:39:18 -0700

    Update Billboard scraper

--- billboard.js
+++ billboard.js
@@ -1,4 +1,26 @@
 //
+// Step 0: check recent scraped
+//
+
+  db.sources.aggregate( [
+    { $match :
+      {
+        parentStream : "The Hot 100"
+      }
+    },
+    { $sort : { publicationDate: -1 } },
+    { $project:
+      {
+        "_id" : 0,
+        "parentEntity" : 0,
+        "parentStream" : 0,
+        "publicationDate" : 0,
+        "location" : 0
+      }
+    }
+  ] ).pretty();
+
+//
 // Step 1: get source data
 //
 
@@ -11,13 +33,17 @@
   publicationDate = document.getElementsByClassName('date-selector__button button--link')[0].innerText.trim();
   publicationDateFormatted = moment(publicationDate, "MMM DD, YYYY").format(); // to ISO
 
+  // URL changes once current chart is archived
+  currentChartLocation = window.location.href + "/" + moment(publicationDate, "MMM DD, YYYY").format("YYYY-MM-DD");
+  pastChartLocation = window.location.href;
+
   // build the object
   source = { // a streamInstance
     "parentEntity": "Billboard",
     "parentStream": "The Hot 100",
     "instanceName": "Week of " + publicationDate,
     "publicationDate": publicationDateFormatted,
-    "location": window.location.href,
+    "location": currentChartLocation, // for current chart only (replace with pastChartLocation if scraping an archived chart)
   };
 
   JSON.stringify(source, null, 4)
@@ -27,7 +53,7 @@
 // Step 2: get songs data
 //
 
-  sourceId = "5eb765066c790abcd85f0fe5" // update with source ID
+  sourceId = "5ebade79d40b0d37890f4e24" // update with source ID
 
   elements = document.getElementsByClassName('chart-list__element display--flex');
 
@@ -57,25 +83,4 @@
 
   JSON.stringify(songs, null, 4)
 
-
-//
-// To see most recent scraped
-//
-
-  db.sources.aggregate( [
-    { $match :
-      {
-        parentStream : "The Hot 100"
-      }
-    },
-    { $sort : { publicationDate: -1 } },
-    { $project:
-      {
-        "_id" : 0,
-        "parentEntity" : 0,
-        "parentStream" : 0,
-        "publicationDate" : 0,
-        "location" : 0
-      }
-    }
-  ] ).pretty();
+  // Remember to update ObjectId(" ") format before inserting songs to the db
