5286df71a1243cf8c81e7047a2a2414377d73d57
davidforrest <davidmforrest@gmail.com>
2020-10-07 15:25:33 -0700

    Update Complex for sqlite

--- billboard.js
+++ billboard.js
@@ -15,7 +15,7 @@ SELECT instance_name FROM source WHERE parent_entity = 'Billboard' ORDER BY publ
 
   // get and format publicationDate
   publicationDate = document.getElementsByClassName('date-selector__button button--link')[0].innerText.trim();
-  publicationDateFormatted = moment(publicationDate, "MMM DD, YYYY").format(); // to ISO
+  publicationDateFormatted = moment(publicationDate, "MMM DD, YYYY").format("YYYY-MM-DD hh:mm:ss.SSSSSS"); // sqlite time format
 
   // URL changes once current chart is archived
   currentChartLocation = window.location.href + "/" + moment(publicationDate, "MMM DD, YYYY").format("YYYY-MM-DD");
@@ -24,11 +24,11 @@ SELECT instance_name FROM source WHERE parent_entity = 'Billboard' ORDER BY publ
   // build the INSERT statement
   console.log(
     "INSERT INTO "
-    + "source (parent_entity, parent_stream, instance_name, publication_date, location) "
-    + "VALUES (\'Billboard\', \'The Hot 100\', \'Week of "
-    + publicationDate + "\', \'"
-    + publicationDateFormatted + "\', \'"
-    + currentChartLocation + "\');"
+    + "source \n  (parent_entity, parent_stream, instance_name, publication_date, location) "
+    + "\nVALUES \n  (\'Billboard\', \'The Hot 100\', \'Week of "
+    + publicationDate + "\', "
+    + "\'" + publicationDateFormatted + "\', "
+    + "\'" + currentChartLocation + "\');"
   )
 
 //
@@ -47,16 +47,17 @@ SELECT instance_name FROM source WHERE parent_entity = 'Billboard' ORDER BY publ
       songName = element.getElementsByClassName('chart-element__information__song text--truncate color--primary')[0];
       artistName = element.getElementsByClassName('chart-element__information__artist text--truncate color--secondary')[0];
 
-      capture_date = moment(new Date()).format();
+      capture_date = moment(new Date()).format("YYYY-MM-DD hh:mm:ss.SSSSSS"); // sqlite time format
       title = songName.innerText.trim();
       artist_name = artistName.innerText.trim();
       video_id = ""; // excluding from SQL statement
 
       song = String(
         "(\'" + capture_date + "\', "
-        + "\'" + source_id + "\', "
+        + source_id + ", "
         + "\'" + title + "\', "
-        + "\'" + artist_name + "\')"
+        + "\'" + artist_name + "\', "
+        + "NULL)"
       );
 
       if(isNew.length == 1 && isNew[0].innerText == "New"){ // because innerText can also be "Re-Enter"
@@ -66,23 +67,20 @@ SELECT instance_name FROM source WHERE parent_entity = 'Billboard' ORDER BY publ
       };
   };
 
-  console.log(songs);
-
-  // Copy into songs.json (scratchpad)
-  // remove all "
+  console.log(String(songs));
 
 //
 // Step 3: paste final statements below:
 //
 
-  INSERT INTO source
-    (parent_entity, parent_stream, instance_name, publication_date, location)
-  VALUES
-    ('Billboard', 'The Hot 100', 'Week of October 10, 2020', '2020-10-10T00:00:00-07:00', 'https://www.billboard.com/charts/hot-100/2020-10-10');
+INSERT INTO source
+  (parent_entity, parent_stream, instance_name, publication_date, location)
+VALUES
+  ('Billboard', 'The Hot 100', 'Week of October 10, 2020', '2020-10-10 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2020-10-10');
 
 
-  INSERT INTO song
-    (capture_date, source_id, title, artist_name)
-  VALUES
-    ('2020-10-06T22:21:10-07:00', '726', 'Franchise', 'Travis Scott Featuring Young Thug & M.I.A.'), ('2020-10-06T22:21:10-07:00', '726', 'Forget Me Too', 'Machine Gun Kelly & Halsey'), ('2020-10-06T22:21:10-07:00', '726', 'Epidemic', 'Polo G'), ('2020-10-06T22:21:10-07:00', '726', 'Bloody Valentine', 'Machine Gun Kelly'), ('2020-10-06T22:21:10-07:00', '726', 'U 2 Luv', 'Ne-Yo & Jeremih'), ('2020-10-06T22:21:10-07:00', '726', 'Better', 'Zayn'), ('2020-10-06T22:21:10-07:00', '726', 'Drunk Face', 'Machine Gun Kelly'), ('2020-10-06T22:21:10-07:00', '726', 'Big, Big Plans', 'Chris Lane'), ('2020-10-06T22:21:10-07:00', '726', 'Ay, Dios Mio!', 'Karol G'), ('2020-10-06T22:21:10-07:00', '726', 'Good Time', 'Niko Moon'), ('2020-10-06T22:21:10-07:00', '726', 'Put Your Records On', 'Ritt Momney'), ('2020-10-06T22:21:10-07:00', '726', 'Money Over Fallouts', 'Tory Lanez')
-  ;
+INSERT INTO song
+  (capture_date, source_id, title, artist_name, video_id)
+VALUES
+  ('2020-10-07 03:16:55.492492', 726, 'Franchise', 'Travis Scott Featuring Young Thug & M.I.A.', NULL),('2020-10-07 03:16:55.494494', 726, 'Forget Me Too', 'Machine Gun Kelly & Halsey', NULL),('2020-10-07 03:16:55.495495', 726, 'Epidemic', 'Polo G', NULL),('2020-10-07 03:16:55.495495', 726, 'Bloody Valentine', 'Machine Gun Kelly', NULL),('2020-10-07 03:16:55.495495', 726, 'U 2 Luv', 'Ne-Yo & Jeremih', NULL),('2020-10-07 03:16:55.495495', 726, 'Better', 'Zayn', NULL),('2020-10-07 03:16:55.495495', 726, 'Drunk Face', 'Machine Gun Kelly', NULL),('2020-10-07 03:16:55.495495', 726, 'Big, Big Plans', 'Chris Lane', NULL),('2020-10-07 03:16:55.495495', 726, 'Ay, Dios Mio!', 'Karol G', NULL),('2020-10-07 03:16:55.495495', 726, 'Good Time', 'Niko Moon', NULL),('2020-10-07 03:16:55.495495', 726, 'Put Your Records On', 'Ritt Momney', NULL),('2020-10-07 03:16:55.495495', 726, 'Money Over Fallouts', 'Tory Lanez', NULL)
+;
--- complex.js
+++ complex.js
@@ -5,23 +5,7 @@
 // Step 0: Check most recent source scraped
 //
 
-  db.sources.aggregate( [
-    { $match :
-      {
-        parentStream : "Best New Music This Week"
-      }
-    },
-    { $sort : { publicationDate: -1 } },
-    { $limit : 3 },
-    { $project:
-      {
-        "_id" : 0,
-        "parentEntity" : 0,
-        "parentStream" : 0,
-        "location" : 0
-      }
-    }
-  ] ).pretty();
+SELECT instance_name, publication_date FROM source WHERE parent_entity = 'Complex' ORDER BY publication_date DESC LIMIT 8;
 
 
 //
@@ -39,50 +23,66 @@
 
   // get and format publicationDate
   publicationDate = document.getElementsByClassName("info-row__datetime")[0].innerHTML.trim();
-  publicationDateFormatted = moment(publicationDate, "MMM DD, YYYY").format(); // to ISO
+  publicationDateFormatted = moment(publicationDate, "MMM DD, YYYY").format("YYYY-MM-DD hh:mm:ss.SSSSSS"); // sqlite time format
 
-  // build the object
-  source = { // a streamInstance
-    "parentEntity": "Complex",
-    "parentStream": parentStream,
-    "instanceName": instanceName,
-    "publicationDate": publicationDateFormatted,
-    "location": window.location.href,
-  };
+  // get and format location
+  chartLocation = window.location.href; // "location" is a reserved word
 
-  JSON.stringify(source, null, 4)
-
+  // build the INSERT statement
+  console.log(
+    "INSERT INTO "
+    + "source \n  (parent_entity, parent_stream, instance_name, publication_date, location) "
+    + "\nVALUES \n  (\'Complex\', "
+    + "\'" + parentStream + "\', "
+    + "\'" + instanceName + "\', "
+    + "\'" + publicationDateFormatted + "\', "
+    + "\'" + chartLocation + "\');"
+  );
 
 //
 // Step 2: get songs data
 //
 
-  sourceId = "5f6ace7e2a27b3a021ae3fdd" // update with source ID
+  source_id = 727 // SELECT last_insert_rowid();
 
-  elements = document.getElementsByClassName("article__copy clearfix");
-  element = elements[0].getElementsByTagName("h3"); // sometimes h2 or h3
+  elements = document.getElementsByClassName("article-list");
+  element = elements[0].getElementsByTagName("h2"); // sometimes h2 or h3
   videoUrl = document.getElementsByClassName("video-lazyload");
 
   songs = [];
 
   for (var i=0; i<element.length; i++){
-    songName = element[i].innerText.match(/, “(.*?)”/)[1]; // may need " or “” type quotation marks, or a comma after the artist name, or may have an &nbsp; instead of a space
-    artistName = element[i].innerText.match(/.+?(?=, “)/)[0]; // may need " or “ type quotation marks
-    videoId = videoUrl[i].style.backgroundImage.match(/(?<=vi\/)(.*)(?=\/)/)[0]; // example: url("https://i.ytimg.com/vi/gejbbL1AaJk/hqdefault.jpg")
+    title = element[i].innerText.match(/, “(.*?)”/)[1]; // may need " or “” type quotation marks, or a comma after the artist name, or may have an &nbsp; instead of a space
+    artist_name = element[i].innerText.match(/.+?(?=, “)/)[0]; // may need " or “ type quotation marks
+    video_id = videoUrl[i].style.backgroundImage.match(/(?<=vi\/)(.*)(?=\/)/)[0]; // example: url("https://i.ytimg.com/vi/gejbbL1AaJk/hqdefault.jpg")
 
-    song = {
-      "captureDate": moment(new Date()).format(),
-      "captureSource": "ObjectId(" + sourceId + ")", // FIXME: should not be a string
-      "songName": songName,
-      "artistName": artistName,
-      "videoId": videoId
-    };
+    capture_date = moment(new Date()).format("YYYY-MM-DD hh:mm:ss.SSSSSS"); // sqlite time format
+
+    song = String(
+      "(\'" + capture_date + "\', "
+      + source_id + ", "
+      + "\'" + title + "\', "
+      + "\'" + artist_name + "\', "
+      + "\'" + video_id + "\')"
+    );
 
     songs.push(song);
 
   };
 
-  JSON.stringify(songs, null, 4)
+  console.log(String(songs));
 
+//
+// Step 3: paste final statements below:
+//
 
-  // format ObjectId("") before adding songs to the db
+INSERT INTO source
+  (parent_entity, parent_stream, instance_name, publication_date, location)
+VALUES
+  ('Complex', 'Best New Music This Week', '21 Savage, Metro Boomin, Megan Thee Stallion, Bryson Tiller, and More', '2020-10-02 12:00:00.000000', 'https://www.complex.com/music/2020/10/best-new-music-this-week-october-2/21-savage-metro-boomin-runnin');
+
+INSERT INTO song
+  (capture_date, source_id, title, artist_name, video_id)
+VALUES
+  ('2020-10-07 03:23:06.957957', 727, 'Runnin', '21 Savage & Metro Boomin', 'jbdROU6eJVg'),('2020-10-07 03:23:06.959959', 727, 'Don’t Stop', 'Megan Thee Stallion f/ Young Thug', '6iuwBio5EUA'),('2020-10-07 03:23:06.959959', 727, 'Outta Time', 'Bryson Tiller f/ Drake', 'FtaW6YMAafk'),('2020-10-07 03:23:06.959959', 727, 'Chocolate Pomegranate', 'Ari Lennox', '0mImzqbi3Qw'),('2020-10-07 03:23:06.959959', 727, 'Big ASAP', 'ASAP Ferg f/ Monica', 'r4xdnCXnRf8'),('2020-10-07 03:23:06.959959', 727, 'All Praises', 'Westside Gunn f/ Boldly James & Jadakiss', 'l3lrYoDU38o'),('2020-10-07 03:23:06.959959', 727, 'Bop It', 'Fivio Foreign f/ Polo G', 'xv10VQcExsw'),('2020-10-07 03:23:06.959959', 727, 'Jealous', 'YG', '2RD_cKtbcfE'),('2020-10-07 03:23:06.959959', 727, 'Bet You Wanna', 'Blackpink f/ Cardi B', 'gXBdvSj9F2I'),('2020-10-07 03:23:06.960960', 727, 'Training Day', 'Dougie F', '4ojvTFUpBes'),('2020-10-07 03:23:06.960960', 727, 'Come Over', 'Jorja Smith f/ Popcaan', 'T9_vgJUylyQ')
+;
