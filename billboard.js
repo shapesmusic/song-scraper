3e5cf883e4f15b515813bbf1eb657ecc31739cb8
davidforrest <davidmforrest@gmail.com>
2020-10-06 22:45:41 -0700

    Update Billboard for sqlite

--- billboard.js
+++ billboard.js
@@ -2,23 +2,7 @@
 // Step 0: check recent scraped
 //
 
-  db.sources.aggregate( [
-    { $match :
-      {
-        parentStream : "The Hot 100"
-      }
-    },
-    { $sort : { publicationDate: -1 } },
-    { $project:
-      {
-        "_id" : 0,
-        "parentEntity" : 0,
-        "parentStream" : 0,
-        "publicationDate" : 0,
-        "location" : 0
-      }
-    }
-  ] ).pretty();
+SELECT instance_name FROM source WHERE parent_entity = 'Billboard' ORDER BY publication_date DESC LIMIT 8;
 
 //
 // Step 1: get source data
@@ -37,23 +21,21 @@
   currentChartLocation = window.location.href + "/" + moment(publicationDate, "MMM DD, YYYY").format("YYYY-MM-DD");
   pastChartLocation = window.location.href;
 
-  // build the object
-  source = { // a streamInstance
-    "parentEntity": "Billboard",
-    "parentStream": "The Hot 100",
-    "instanceName": "Week of " + publicationDate,
-    "publicationDate": publicationDateFormatted,
-    "location": currentChartLocation, // for current chart only (replace with pastChartLocation if scraping an archived chart)
-  };
-
-  JSON.stringify(source, null, 4)
-
+  // build the INSERT statement
+  console.log(
+    "INSERT INTO "
+    + "source (parent_entity, parent_stream, instance_name, publication_date, location) "
+    + "VALUES (\'Billboard\', \'The Hot 100\', \'Week of "
+    + publicationDate + "\', \'"
+    + publicationDateFormatted + "\', \'"
+    + currentChartLocation + "\');"
+  )
 
 //
 // Step 2: get songs data
 //
 
-  sourceId = "5f6a8907f167c173664e102d" // update with source ID
+  source_id = 726 // SELECT last_insert_rowid();
 
   elements = document.getElementsByClassName('chart-list__element display--flex');
 
@@ -65,22 +47,42 @@
       songName = element.getElementsByClassName('chart-element__information__song text--truncate color--primary')[0];
       artistName = element.getElementsByClassName('chart-element__information__artist text--truncate color--secondary')[0];
 
-      song = {
-        "captureDate": moment(new Date()).format(),
-        "captureSource": "ObjectId(" + sourceId + ")", // FIXME: should not be a string
-        "songName": songName.innerText.trim(),
-        "artistName": artistName.innerText.trim(),
-        "videoId": ""
-      };
+      capture_date = moment(new Date()).format();
+      title = songName.innerText.trim();
+      artist_name = artistName.innerText.trim();
+      video_id = ""; // excluding from SQL statement
+
+      song = String(
+        "(\'" + capture_date + "\', "
+        + "\'" + source_id + "\', "
+        + "\'" + title + "\', "
+        + "\'" + artist_name + "\')"
+      );
 
       if(isNew.length == 1 && isNew[0].innerText == "New"){ // because innerText can also be "Re-Enter"
 
         songs.push(song);
 
       };
-
   };
 
-  JSON.stringify(songs, null, 4)
+  console.log(songs);
 
-  // Remember to update ObjectId(" ") format before inserting songs to the db
+  // Copy into songs.json (scratchpad)
+  // remove all "
+
+//
+// Step 3: paste final statements below:
+//
+
+  INSERT INTO source
+    (parent_entity, parent_stream, instance_name, publication_date, location)
+  VALUES
+    ('Billboard', 'The Hot 100', 'Week of October 10, 2020', '2020-10-10T00:00:00-07:00', 'https://www.billboard.com/charts/hot-100/2020-10-10');
+
+
+  INSERT INTO song
+    (capture_date, source_id, title, artist_name)
+  VALUES
+    ('2020-10-06T22:21:10-07:00', '726', 'Franchise', 'Travis Scott Featuring Young Thug & M.I.A.'), ('2020-10-06T22:21:10-07:00', '726', 'Forget Me Too', 'Machine Gun Kelly & Halsey'), ('2020-10-06T22:21:10-07:00', '726', 'Epidemic', 'Polo G'), ('2020-10-06T22:21:10-07:00', '726', 'Bloody Valentine', 'Machine Gun Kelly'), ('2020-10-06T22:21:10-07:00', '726', 'U 2 Luv', 'Ne-Yo & Jeremih'), ('2020-10-06T22:21:10-07:00', '726', 'Better', 'Zayn'), ('2020-10-06T22:21:10-07:00', '726', 'Drunk Face', 'Machine Gun Kelly'), ('2020-10-06T22:21:10-07:00', '726', 'Big, Big Plans', 'Chris Lane'), ('2020-10-06T22:21:10-07:00', '726', 'Ay, Dios Mio!', 'Karol G'), ('2020-10-06T22:21:10-07:00', '726', 'Good Time', 'Niko Moon'), ('2020-10-06T22:21:10-07:00', '726', 'Put Your Records On', 'Ritt Momney'), ('2020-10-06T22:21:10-07:00', '726', 'Money Over Fallouts', 'Tory Lanez')
+  ;
