d943f02ed5b26ae6efe6ab25d82ea7e74209caef
davidforrest <dforrest@g.harvard.edu>
2020-12-22 15:06:43 -0800

    Update NYT for 2020-11-20

--- nytPlaylist.js
+++ nytPlaylist.js
@@ -43,7 +43,7 @@
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('New York Times', 'The Playlist', 'Billie Eilish’s Kiss-Off, and 14 More New Songs', '2020-11-13 09:00:16.000000', 'https://www.nytimes.com/2020/11/13/arts/music/playlist-billie-eilish-lil-nas-x.html');
+    ('New York Times', 'The Playlist', 'Miley Cyrus and Dua Lipa’s Retro Rock, and 8 More New Songs', '2020-11-20 08:09:02.000000', 'https://www.nytimes.com/2020/11/20/arts/music/playlist-miley-cyrus-dua-lipa-megan-thee-stallion.html');
 
   // Update to source table
 
@@ -52,7 +52,7 @@
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 748; // SELECT last_insert_rowid();
+  source_id = 758; // SELECT last_insert_rowid();
   song_id = null;
 
   elements = document.getElementsByClassName("css-ow6j0y eoo0vm40"); // this class changes periodically
@@ -95,96 +95,87 @@
   songsData =
   [
     {
-        "title": "Therefore I Am",
-        "artist_name": "Billie Eilish",
+        "title": "Prisoner",
+        "artist_name": "Miley Cyrus featuring Dua Lipa",
         "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.867867",
-        "source_id": 748,
-        "song_id": 9565,
+        "capture_date": "2020-12-22 03:01:13.605605",
+        "source_id": 758,
+        "song_id": 9598,
         "duplicate": true
     },
     {
-        "title": "Shame Shame",
-        "artist_name": "Foo Fighters",
+        "title": "﻿Harlem River Blues",
+        "artist_name": "Steve Earle & the Dukes",
         "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.868868",
-        "source_id": 748,
+        "capture_date": "2020-12-22 03:01:13.606606",
+        "source_id": 758,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Holiday",
-        "artist_name": "Lil Nas X",
+        "title": "Chinatown",
+        "artist_name": "Bleachers featuring Bruce Springsteen",
         "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.868868",
-        "source_id": 748,
-        "song_id": 9571,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Galaxy 1000",
+        "artist_name": "Rob Mazurek’s Exploding Star Orchestra",
+        "video_id": null,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Scratchcard Lanyard",
+        "artist_name": "Dry Cleaning",
+        "video_id": null,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Body",
+        "artist_name": "Megan Thee Stallion",
+        "video_id": null,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
+        "song_id": 9593,
         "duplicate": true
     },
     {
-        "title": "Ginger",
-        "artist_name": "Wizkid featuring Burna Boy",
+        "title": "Monster",
+        "artist_name": "Shawn Mendes and Justin Bieber",
         "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.868868",
-        "source_id": 748,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Iris",
-        "artist_name": "Phoebe Bridgers and Maggie Rogers",
-        "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.869869",
-        "source_id": 748,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Hole in the Bottle",
-        "artist_name": "Kelsea Ballerini ft. Shania Twain",
-        "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.869869",
-        "source_id": 748,
-        "song_id": 9406,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
+        "song_id": 9592,
         "duplicate": true
     },
     {
-        "title": "You Should Probably Leave",
-        "artist_name": "Chris Stapleton",
+        "title": "Kyoto (Copycat Killer Version)",
+        "artist_name": "Phoebe Bridgers",
         "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.869869",
-        "source_id": 748,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Realize",
-        "artist_name": "AC/DC",
+        "title": "Hell for Certain",
+        "artist_name": "Gwenifer Raymond",
         "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.869869",
-        "source_id": 748,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "No Save Point",
-        "artist_name": "Run the Jewels",
-        "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.869869",
-        "source_id": 748,
-        "song_id": 9570,
-        "duplicate": true
-    },
-    {
-        "title": "Chasing Light",
-        "artist_name": "Emily Sprague",
-        "video_id": null,
-        "capture_date": "2020-11-19 09:58:19.869869",
-        "source_id": 748,
+        "capture_date": "2020-12-22 03:01:13.607607",
+        "source_id": 758,
         "song_id": null,
         "duplicate": false
     }
-  ]
+]
 
   // Check each song for duplicates in the database
   SELECT id, title, artist_name FROM song WHERE
@@ -223,12 +214,12 @@
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-    ('Shame Shame', 'Foo Fighters', NULL),
-    ('Ginger', 'Wizkid featuring Burna Boy', NULL),
-    ('Iris', 'Phoebe Bridgers and Maggie Rogers', NULL),
-    ('You Should Probably Leave', 'Chris Stapleton', NULL),
-    ('Realize', 'AC/DC', NULL),
-    ('Chasing Light', 'Emily Sprague', NULL)
+    ('﻿Harlem River Blues', 'Steve Earle & the Dukes', NULL),
+    ('Chinatown', 'Bleachers featuring Bruce Springsteen', NULL),
+    ('Galaxy 1000', 'Rob Mazurek’s Exploding Star Orchestra', NULL),
+    ('Scratchcard Lanyard', 'Dry Cleaning', NULL),
+    ('Kyoto (Copycat Killer Version)', 'Phoebe Bridgers', NULL),
+    ('Hell for Certain', 'Gwenifer Raymond', NULL)
   ;
 
    // Update to song table
@@ -239,7 +230,7 @@
  //
 
   // Get the last song_id inserted
-  song_id = 9580; // SELECT last_insert_rowid();
+  song_id = 9690; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -279,16 +270,15 @@
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-    ('2020-11-19 09:58:19.867867', '748', '9565'),
-    ('2020-11-19 09:58:19.868868', '748', '9575'),
-    ('2020-11-19 09:58:19.868868', '748', '9571'),
-    ('2020-11-19 09:58:19.868868', '748', '9576'),
-    ('2020-11-19 09:58:19.869869', '748', '9577'),
-    ('2020-11-19 09:58:19.869869', '748', '9406'),
-    ('2020-11-19 09:58:19.869869', '748', '9578'),
-    ('2020-11-19 09:58:19.869869', '748', '9579'),
-    ('2020-11-19 09:58:19.869869', '748', '9570'),
-    ('2020-11-19 09:58:19.869869', '748', '9580')
+    ('2020-12-22 03:01:13.605605', '758', '9598'),
+    ('2020-12-22 03:01:13.606606', '758', '9685'),
+    ('2020-12-22 03:01:13.607607', '758', '9686'),
+    ('2020-12-22 03:01:13.607607', '758', '9687'),
+    ('2020-12-22 03:01:13.607607', '758', '9688'),
+    ('2020-12-22 03:01:13.607607', '758', '9593'),
+    ('2020-12-22 03:01:13.607607', '758', '9592'),
+    ('2020-12-22 03:01:13.607607', '758', '9689'),
+    ('2020-12-22 03:01:13.607607', '758', '9690')
   ;
 
   // Update to source_song table
