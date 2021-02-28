4dd6517d9f30f5432b3c861e5c68e0445c073154
davidforrest <davidmforrest@gmail.com>
2021-02-27 20:26:12 -0800

    Update Billboard for 2021-02-13

--- billboard.js
+++ billboard.js
@@ -39,7 +39,7 @@
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('Billboard', 'The Hot 100', 'Week of February 6, 2021', '2021-02-06 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-02-06');
+    ('Billboard', 'The Hot 100', 'Week of February 13, 2021', '2021-02-13 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-02-13');
 
   // Update to source table
 
@@ -48,7 +48,7 @@
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 769; // SELECT last_insert_rowid();
+  source_id = 770; // SELECT last_insert_rowid();
   song_id = null;
 
   elements = document.getElementsByClassName('chart-list__element display--flex');
@@ -93,38 +93,56 @@
   songsData =
   [
     {
-        "title": "Skin",
-        "artist_name": "Sabrina Carpenter",
+        "title": "Finesse Out The Gang Way",
+        "artist_name": "Lil Durk Featuring Lil Baby",
         "video_id": null,
-        "capture_date": "2021-02-27 08:17:07.873873",
-        "source_id": 769,
+        "capture_date": "2021-02-27 08:23:17.500500",
+        "source_id": 770,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Lo Vas A Olvidar",
-        "artist_name": "Billie Eilish & ROSALIA",
+        "title": "Should've Ducked",
+        "artist_name": "Lil Durk Featuring Pooh Shiesty",
         "video_id": null,
-        "capture_date": "2021-02-27 08:17:07.873873",
-        "source_id": 769,
+        "capture_date": "2021-02-27 08:23:17.501501",
+        "source_id": 770,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Opp Stoppa",
-        "artist_name": "YBN Nahmir Featuring 21 Savage",
+        "title": "Gravity",
+        "artist_name": "Brent Faiyaz & DJ Dahi Featuring Tyler, The Creator",
         "video_id": null,
-        "capture_date": "2021-02-27 08:17:07.880880",
-        "source_id": 769,
+        "capture_date": "2021-02-27 08:23:17.502502",
+        "source_id": 770,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Antes",
-        "artist_name": "Anuel AA & Ozuna",
+        "title": "Baila Conmigo",
+        "artist_name": "Selena Gomez With Rauw Alejandro",
         "video_id": null,
-        "capture_date": "2021-02-27 08:17:07.881881",
-        "source_id": 769,
+        "capture_date": "2021-02-27 08:23:17.502502",
+        "source_id": 770,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Kanye Krazy",
+        "artist_name": "Lil Durk",
+        "video_id": null,
+        "capture_date": "2021-02-27 08:23:17.503503",
+        "source_id": 770,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Fake Woke",
+        "artist_name": "Tom MacDonald",
+        "video_id": null,
+        "capture_date": "2021-02-27 08:23:17.503503",
+        "source_id": 770,
         "song_id": null,
         "duplicate": false
     }
@@ -167,10 +185,12 @@
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-  ('Skin', 'Sabrina Carpenter', NULL),
-  ('Lo Vas A Olvidar', 'Billie Eilish & ROSALIA', NULL),
-  ('Opp Stoppa', 'YBN Nahmir Featuring 21 Savage', NULL),
-  ('Antes', 'Anuel AA & Ozuna', NULL)
+  ('Finesse Out The Gang Way', 'Lil Durk Featuring Lil Baby', NULL),
+  ('Should’ve Ducked', 'Lil Durk Featuring Pooh Shiesty', NULL),
+  ('Gravity', 'Brent Faiyaz & DJ Dahi Featuring Tyler, The Creator', NULL),
+  ('Baila Conmigo', 'Selena Gomez With Rauw Alejandro', NULL),
+  ('Kanye Krazy', 'Lil Durk', NULL),
+  ('Fake Woke', 'Tom MacDonald', NULL)
   ;
 
    // Update to song table
@@ -181,7 +201,7 @@
  //
 
   // Get the last song_id inserted
-  song_id = 9767; // SELECT last_insert_rowid();
+  song_id = 9773; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -221,10 +241,12 @@
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-  ('2021-02-27 08:17:07.873873', '769', '9764'),
-  ('2021-02-27 08:17:07.873873', '769', '9765'),
-  ('2021-02-27 08:17:07.880880', '769', '9766'),
-  ('2021-02-27 08:17:07.881881', '769', '9767')
+  ('2021-02-27 08:23:17.500500', '770', '9768'),
+  ('2021-02-27 08:23:17.501501', '770', '9769'),
+  ('2021-02-27 08:23:17.502502', '770', '9770'),
+  ('2021-02-27 08:23:17.502502', '770', '9771'),
+  ('2021-02-27 08:23:17.503503', '770', '9772'),
+  ('2021-02-27 08:23:17.503503', '770', '9773')
   ;
 
   // Update to source_song table
