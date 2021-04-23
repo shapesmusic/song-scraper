b2c8f9669cfea04aab3f74b62dd5e741fc531405
davidforrest <davidmforrest@gmail.com>
2021-04-22 21:31:19 -0700

    Update YT for 2021-04-08

--- yt.js
+++ yt.js
@@ -45,7 +45,7 @@ pastChartLocation = window.location.href;
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('YouTube', 'Global Top Songs', 'Week of Apr 1, 2021', '2021-04-01 12:00:00.000000', 'https://charts.youtube.com/charts/TopSongs/global/20210326-20210401');
+    ('YouTube', 'Global Top Songs', 'Week of Apr 8, 2021', '2021-04-08 12:00:00.000000', 'https://charts.youtube.com/charts/TopSongs/global/20210402-20210408');
 
   // Update to source table
 
@@ -54,7 +54,7 @@ pastChartLocation = window.location.href;
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 876; // SELECT last_insert_rowid();
+  source_id = 886; // SELECT last_insert_rowid();
   song_id = null;
 
   elements = document.getElementsByClassName('chart-table-row style-scope ytmc-chart-table');
@@ -105,184 +105,211 @@ pastChartLocation = window.location.href;
   songsData =
   [
     {
-        "title": "MONTERO (Call Me By Your Name)",
-        "artist_name": "Lil Nas X",
+        "title": "deja vu",
+        "artist_name": "Olivia Rodrigo",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.912912",
-        "source_id": 876,
-        "song_id": 10082,
+        "capture_date": "2021-04-22 05:28:47.032032",
+        "source_id": 886,
+        "song_id": 10142,
         "duplicate": true
     },
     {
-        "title": "Hori Khele Raghuveera",
-        "artist_name": "Alka Yagnik, Udit Narayan, Amitabh Bachchan & Sukhwinder Singh",
+        "title": "Pani Di Gal",
+        "artist_name": "Maninder Buttar",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.913913",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.033033",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "EL MAKINON",
-        "artist_name": "Karol G & Mariah Angeliq",
+        "title": "Galat",
+        "artist_name": "Asees Kaur",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.914914",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.033033",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Baarish Ki Jaaye",
-        "artist_name": "B Praak feat. Nawazuddin Siddiqui & Sunanda Sharma",
+        "title": "Lehja",
+        "artist_name": "Abhi Dutt",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.914914",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.034034",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Film out",
-        "artist_name": "BTS",
+        "title": "La Curiosidad",
+        "artist_name": "Jay Wheeler",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.916916",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.034034",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "CHO MÌNH EM",
-        "artist_name": "Binz & Đen",
+        "title": "CHỈ LÀ KHÔNG CÙNG NHAU (Live)",
+        "artist_name": "Tăng Phúc feat. Trương Thảo Nhi",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.916916",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.034034",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Kismat Teri",
-        "artist_name": "Inder Chahal",
+        "title": "Dancing With the Devil",
+        "artist_name": "Demi Lovato",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.916916",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Kekra Sang Khelme Holiya Ge Jaan",
-        "artist_name": "Prabha Raj",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.916916",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Balam Pichkari",
-        "artist_name": "Shalmali Kholgade & Vishal Dadlani",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.917917",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Loot Liya",
-        "artist_name": "Khasa Aala Chahar",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.917917",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "MUSAFIR",
-        "artist_name": "Korala Maan & Gurlez Akhtar",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.917917",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Rang Barse",
-        "artist_name": "Amitabh Bachchan",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.918918",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Tujhe Bhoolna Toh Chaaha",
-        "artist_name": "Rochak Kohli & Jubin Nautiyal",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.918918",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "No Hago Coro",
-        "artist_name": "GhettoSPM, Farruko & Nino Freestyle",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.918918",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "EL BARCO",
-        "artist_name": "Karol G",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.919919",
-        "source_id": 876,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Tombstone",
-        "artist_name": "Rod Wave",
-        "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.919919",
-        "source_id": 876,
-        "song_id": 10062,
+        "capture_date": "2021-04-22 05:28:47.035035",
+        "source_id": 886,
+        "song_id": 10183,
         "duplicate": true
     },
     {
-        "title": "La Llevo Al Cielo",
-        "artist_name": "Kallde \"El Rey Del Placer\" feat. Chencho Corleone",
+        "title": "Runaway",
+        "artist_name": "AURORA",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.919919",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.035035",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Take a picture",
-        "artist_name": "NiziU",
+        "title": "Nishaan",
+        "artist_name": "Kaka",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.919919",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.035035",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Kutty Pattas",
-        "artist_name": "Santhosh Dhayanidhi & Rakshitha Suresh",
+        "title": "WACHA",
+        "artist_name": "KHEA",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.919919",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.036036",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "廻廻奇譚",
-        "artist_name": "Eve",
+        "title": "Новая Волна",
+        "artist_name": "DJ Smash",
         "video_id": null,
-        "capture_date": "2021-04-06 08:12:10.920920",
-        "source_id": 876,
+        "capture_date": "2021-04-22 05:28:47.036036",
+        "source_id": 886,
         "song_id": null,
         "duplicate": false
+    },
+    {
+        "title": "Thoda Thoda Pyaar",
+        "artist_name": "Stebin Ben",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.036036",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Talking to the Moon",
+        "artist_name": "Bruno Mars",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.036036",
+        "source_id": 886,
+        "song_id": 10079,
+        "duplicate": true
+    },
+    {
+        "title": "El Efecto",
+        "artist_name": "Rauw Alejandro",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.037037",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Calling My Phone",
+        "artist_name": "Lil Tjay feat. 6LACK",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.037037",
+        "source_id": 886,
+        "song_id": 9786,
+        "duplicate": true
+    },
+    {
+        "title": "Vroum Vroum",
+        "artist_name": "Moha K",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.037037",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "First Day Out (Beat Box)",
+        "artist_name": "NLE Choppa",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.037037",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Bismillah Cinta",
+        "artist_name": "Ungu, Lesti",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.038038",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Patoreakcja",
+        "artist_name": "Mata",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.038038",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Somebody That I Used To Know",
+        "artist_name": "Gotye",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.038038",
+        "source_id": 886,
+        "song_id": 5580,
+        "duplicate": true
+    },
+    {
+        "title": "Wellerman (Sea Shanty)",
+        "artist_name": "Nathan Evans",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.038038",
+        "source_id": 886,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Track Star",
+        "artist_name": "Mooski",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.038038",
+        "source_id": 886,
+        "song_id": 9943,
+        "duplicate": true
+    },
+    {
+        "title": "Sweater Weather",
+        "artist_name": "The Neighbourhood",
+        "video_id": null,
+        "capture_date": "2021-04-22 05:28:47.039039",
+        "source_id": 886,
+        "song_id": 4778,
+        "duplicate": true
     }
 ]
 
@@ -323,24 +350,22 @@ pastChartLocation = window.location.href;
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-  ('Hori Khele Raghuveera', 'Alka Yagnik, Udit Narayan, Amitabh Bachchan & Sukhwinder Singh', NULL),
-  ('EL MAKINON', 'Karol G & Mariah Angeliq', NULL),
-  ('Baarish Ki Jaaye', 'B Praak feat. Nawazuddin Siddiqui & Sunanda Sharma', NULL),
-  ('Film out', 'BTS', NULL),
-  ('CHO MÌNH EM', 'Binz & Đen', NULL),
-  ('Kismat Teri', 'Inder Chahal', NULL),
-  ('Kekra Sang Khelme Holiya Ge Jaan', 'Prabha Raj', NULL),
-  ('Balam Pichkari', 'Shalmali Kholgade & Vishal Dadlani', NULL),
-  ('Loot Liya', 'Khasa Aala Chahar', NULL),
-  ('MUSAFIR', 'Korala Maan & Gurlez Akhtar', NULL),
-  ('Rang Barse', 'Amitabh Bachchan', NULL),
-  ('Tujhe Bhoolna Toh Chaaha', 'Rochak Kohli & Jubin Nautiyal', NULL),
-  ('No Hago Coro', 'GhettoSPM, Farruko & Nino Freestyle', NULL),
-  ('EL BARCO', 'Karol G', NULL),
-  ('La Llevo Al Cielo', 'Kallde "El Rey Del Placer" feat. Chencho Corleone', NULL),
-  ('Take a picture', 'NiziU', NULL),
-  ('Kutty Pattas', 'Santhosh Dhayanidhi & Rakshitha Suresh', NULL),
-  ('廻廻奇譚', 'Eve', NULL)
+  ('Pani Di Gal', 'Maninder Buttar', NULL),
+  ('Galat', 'Asees Kaur', NULL),
+  ('Lehja', 'Abhi Dutt', NULL),
+  ('La Curiosidad', 'Jay Wheeler', NULL),
+  ('CHỈ LÀ KHÔNG CÙNG NHAU (Live)', 'Tăng Phúc feat. Trương Thảo Nhi', NULL),
+  ('Runaway', 'AURORA', NULL),
+  ('Nishaan', 'Kaka', NULL),
+  ('WACHA', 'KHEA', NULL),
+  ('Новая Волна', 'DJ Smash', NULL),
+  ('Thoda Thoda Pyaar', 'Stebin Ben', NULL),
+  ('El Efecto', 'Rauw Alejandro', NULL),
+  ('Vroum Vroum', 'Moha K', NULL),
+  ('First Day Out (Beat Box)', 'NLE Choppa', NULL),
+  ('Bismillah Cinta', 'Ungu, Lesti', NULL),
+  ('Patoreakcja', 'Mata', NULL),
+  ('Wellerman (Sea Shanty)', 'Nathan Evans', NULL)
   ;
 
    // Update to song table
@@ -351,7 +376,7 @@ pastChartLocation = window.location.href;
  //
 
   // Get the last song_id inserted
-  song_id = 10135; // SELECT last_insert_rowid();
+  song_id = 10218; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -391,26 +416,29 @@ pastChartLocation = window.location.href;
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-  ('2021-04-06 08:12:10.912912', '876', '10082'),
-  ('2021-04-06 08:12:10.913913', '876', '10118'),
-  ('2021-04-06 08:12:10.914914', '876', '10119'),
-  ('2021-04-06 08:12:10.914914', '876', '10120'),
-  ('2021-04-06 08:12:10.916916', '876', '10121'),
-  ('2021-04-06 08:12:10.916916', '876', '10122'),
-  ('2021-04-06 08:12:10.916916', '876', '10123'),
-  ('2021-04-06 08:12:10.916916', '876', '10124'),
-  ('2021-04-06 08:12:10.917917', '876', '10125'),
-  ('2021-04-06 08:12:10.917917', '876', '10126'),
-  ('2021-04-06 08:12:10.917917', '876', '10127'),
-  ('2021-04-06 08:12:10.918918', '876', '10128'),
-  ('2021-04-06 08:12:10.918918', '876', '10129'),
-  ('2021-04-06 08:12:10.918918', '876', '10130'),
-  ('2021-04-06 08:12:10.919919', '876', '10131'),
-  ('2021-04-06 08:12:10.919919', '876', '10062'),
-  ('2021-04-06 08:12:10.919919', '876', '10132'),
-  ('2021-04-06 08:12:10.919919', '876', '10133'),
-  ('2021-04-06 08:12:10.919919', '876', '10134'),
-  ('2021-04-06 08:12:10.920920', '876', '10135')
+  ('2021-04-22 05:28:47.032032', '886', '10142'),
+  ('2021-04-22 05:28:47.033033', '886', '10203'),
+  ('2021-04-22 05:28:47.033033', '886', '10204'),
+  ('2021-04-22 05:28:47.034034', '886', '10205'),
+  ('2021-04-22 05:28:47.034034', '886', '10206'),
+  ('2021-04-22 05:28:47.034034', '886', '10207'),
+  ('2021-04-22 05:28:47.035035', '886', '10183'),
+  ('2021-04-22 05:28:47.035035', '886', '10208'),
+  ('2021-04-22 05:28:47.035035', '886', '10209'),
+  ('2021-04-22 05:28:47.036036', '886', '10210'),
+  ('2021-04-22 05:28:47.036036', '886', '10211'),
+  ('2021-04-22 05:28:47.036036', '886', '10212'),
+  ('2021-04-22 05:28:47.036036', '886', '10079'),
+  ('2021-04-22 05:28:47.037037', '886', '10213'),
+  ('2021-04-22 05:28:47.037037', '886', '9786'),
+  ('2021-04-22 05:28:47.037037', '886', '10214'),
+  ('2021-04-22 05:28:47.037037', '886', '10215'),
+  ('2021-04-22 05:28:47.038038', '886', '10216'),
+  ('2021-04-22 05:28:47.038038', '886', '10217'),
+  ('2021-04-22 05:28:47.038038', '886', '5580'),
+  ('2021-04-22 05:28:47.038038', '886', '10218'),
+  ('2021-04-22 05:28:47.038038', '886', '9943'),
+  ('2021-04-22 05:28:47.039039', '886', '4778')
   ;
 
   // Update to source_song table
