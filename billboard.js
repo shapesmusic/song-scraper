f2ff1ea3eed52a2bc4cd9478686644a9a30aec14
davidforrest <davidmforrest@gmail.com>
2021-10-10 22:55:47 -0400

    Update Billboard for 2021-06-26

--- billboard.js
+++ billboard.js
@@ -39,7 +39,7 @@
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('Billboard', 'The Hot 100', 'Week of June 19, 2021', '2021-06-19 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-06-19');
+    ('Billboard', 'The Hot 100', 'Week of June 26, 2021', '2021-06-26 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-06-26/2021-06-26');
 
   // Update to source table
 
@@ -48,7 +48,7 @@
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 938; // SELECT last_insert_rowid();
+  source_id = 942; // SELECT last_insert_rowid();
   song_id = null;
 
   elements = document.getElementsByClassName('chart-list__element display--flex');
@@ -96,173 +96,191 @@
   songsData =
   [
     {
-        "title": "Yonaguni",
-        "artist_name": "Bad Bunny",
+        "title": "Having Our Way",
+        "artist_name": "Migos ft. Drake",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.687687",
-        "source_id": 938,
-        "song_id": 10517,
+        "capture_date": "2021-10-10 10:44:03.378378",
+        "source_id": 942,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Thot Shit",
+        "artist_name": "Megan Thee Stallion",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.378378",
+        "source_id": 942,
+        "song_id": 10573,
         "duplicate": true
     },
     {
-        "title": "Hats Off",
-        "artist_name": "Lil Baby, Lil Durk & Travis Scott",
+        "title": "No Return",
+        "artist_name": "Polo G ft. The Kid LAROI & Lil Durk",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.687687",
-        "source_id": 938,
-        "song_id": 10513,
+        "capture_date": "2021-10-10 10:44:03.379379",
+        "source_id": 942,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Avalanche",
+        "artist_name": "Migos",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.379379",
+        "source_id": 942,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Need To Know",
+        "artist_name": "Doja Cat",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.380380",
+        "source_id": 942,
+        "song_id": 10577,
         "duplicate": true
     },
     {
-        "title": "Late At Night",
-        "artist_name": "Roddy Ricch",
+        "title": "Black Hearted",
+        "artist_name": "Polo G",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.687687",
-        "source_id": 938,
-        "song_id": 10514,
+        "capture_date": "2021-10-10 10:44:03.380380",
+        "source_id": 942,
+        "song_id": 10574,
         "duplicate": true
     },
     {
-        "title": "Lost Cause",
-        "artist_name": "Billie Eilish",
+        "title": "Painting Pictures",
+        "artist_name": "Polo G",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.687687",
-        "source_id": 938,
-        "song_id": 10536,
+        "capture_date": "2021-10-10 10:44:03.381381",
+        "source_id": 942,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Modern Day",
+        "artist_name": "Migos",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.381381",
+        "source_id": 942,
+        "song_id": 10572,
         "duplicate": true
     },
     {
-        "title": "2040",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "Toxic",
+        "artist_name": "Polo G",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.688688",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.381381",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "How It Feels",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "Solar Power",
+        "artist_name": "Lorde",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.688688",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.381381",
+        "source_id": 942,
+        "song_id": 10581,
+        "duplicate": true
+    },
+    {
+        "title": "Malibu",
+        "artist_name": "Migos ft. Polo G",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.381381",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Still Runnin",
-        "artist_name": "Lil Baby, Lil Durk & Meek Mill",
+        "title": "Type Shit",
+        "artist_name": "Migos & Cardi B",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.688688",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.381381",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Who I Want",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "Clueless",
+        "artist_name": "Polo G ft. Pop Smoke & Fivio Foreign",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.688688",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Still Hood",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "Heart Of A Giant",
+        "artist_name": "Polo G ft. Rod Wave",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.688688",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Okay",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "Party Lyfe",
+        "artist_name": "Polo G ft. DaBaby",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.688688",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Man Of My Word",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "Go Part 1",
+        "artist_name": "Polo G ft. G Herbo",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Medical",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "transparentsoul",
+        "artist_name": "Willow ft. Travis Barker",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
+        "song_id": 10329,
+        "duplicate": true
+    },
+    {
+        "title": "Boom",
+        "artist_name": "Polo G",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Rich Off Pain",
-        "artist_name": "Lil Baby, Lil Durk & Rod Wave",
+        "title": "Fiel",
+        "artist_name": "Los Legendarios, Wisin & Jhay Cortez",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.382382",
+        "source_id": 942,
+        "song_id": 10072,
+        "duplicate": true
+    },
+    {
+        "title": "Bloody Canvas",
+        "artist_name": "Polo G",
+        "video_id": null,
+        "capture_date": "2021-10-10 10:44:03.383383",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Lying",
-        "artist_name": "Lil Baby & Lil Durk",
+        "title": "You",
+        "artist_name": "Regard x Troye Sivan x Tate McRae",
         "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Snowflakes",
-        "artist_name": "Tom MacDonald",
-        "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "That's Facts",
-        "artist_name": "Lil Baby & Lil Durk",
-        "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Please",
-        "artist_name": "Lil Baby & Lil Durk",
-        "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "Up The Side",
-        "artist_name": "Lil Baby, Lil Durk & Young Thug",
-        "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.689689",
-        "source_id": 938,
-        "song_id": null,
-        "duplicate": false
-    },
-    {
-        "title": "If You Want To",
-        "artist_name": "Lil Baby & Lil Durk",
-        "video_id": null,
-        "capture_date": "2021-06-17 11:58:18.690690",
-        "source_id": 938,
+        "capture_date": "2021-10-10 10:44:03.383383",
+        "source_id": 942,
         "song_id": null,
         "duplicate": false
     }
@@ -305,21 +323,20 @@
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-  ('2040', 'Lil Baby & Lil Durk', NULL),
-  ('How It Feels', 'Lil Baby & Lil Durk', NULL),
-  ('Still Runnin', 'Lil Baby, Lil Durk & Meek Mill', NULL),
-  ('Who I Want', 'Lil Baby & Lil Durk', NULL),
-  ('Still Hood', 'Lil Baby & Lil Durk', NULL),
-  ('Okay', 'Lil Baby & Lil Durk', NULL),
-  ('Man Of My Word', 'Lil Baby & Lil Durk', NULL),
-  ('Medical', 'Lil Baby & Lil Durk', NULL),
-  ('Rich Off Pain', 'Lil Baby, Lil Durk & Rod Wave', NULL),
-  ('Lying', 'Lil Baby & Lil Durk', NULL),
-  ('Snowflakes', 'Tom MacDonald', NULL),
-  ('That’s Facts', 'Lil Baby & Lil Durk', NULL),
-  ('Please', 'Lil Baby & Lil Durk', NULL),
-  ('Up The Side', 'Lil Baby, Lil Durk & Young Thug', NULL),
-  ('If You Want To', 'Lil Baby & Lil Durk', NULL)
+  ('Having Our Way', 'Migos ft. Drake', NULL),
+  ('No Return', 'Polo G ft. The Kid LAROI & Lil Durk', NULL),
+  ('Avalanche', 'Migos', NULL),
+  ('Painting Pictures', 'Polo G', NULL),
+  ('Toxic', 'Polo G', NULL),
+  ('Malibu', 'Migos ft. Polo G', NULL),
+  ('Type Shit', 'Migos & Cardi B', NULL),
+  ('Clueless', 'Polo G ft. Pop Smoke & Fivio Foreign', NULL),
+  ('Heart Of A Giant', 'Polo G ft. Rod Wave', NULL),
+  ('Party Lyfe', 'Polo G ft. DaBaby', NULL),
+  ('Go Part 1', 'Polo G ft. G Herbo', NULL),
+  ('Boom', 'Polo G', NULL),
+  ('Bloody Canvas', 'Polo G', NULL),
+  ('You', 'Regard x Troye Sivan x Tate McRae', NULL)
   ;
 
    // Update to song table
@@ -330,7 +347,7 @@
  //
 
   // Get the last song_id inserted
-  song_id = 10571; // SELECT last_insert_rowid();
+  song_id = 10621; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -370,25 +387,27 @@
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-  ('2021-06-17 11:58:18.687687', '938', '10517'),
-  ('2021-06-17 11:58:18.687687', '938', '10513'),
-  ('2021-06-17 11:58:18.687687', '938', '10514'),
-  ('2021-06-17 11:58:18.687687', '938', '10536'),
-  ('2021-06-17 11:58:18.688688', '938', '10557'),
-  ('2021-06-17 11:58:18.688688', '938', '10558'),
-  ('2021-06-17 11:58:18.688688', '938', '10559'),
-  ('2021-06-17 11:58:18.688688', '938', '10560'),
-  ('2021-06-17 11:58:18.688688', '938', '10561'),
-  ('2021-06-17 11:58:18.688688', '938', '10562'),
-  ('2021-06-17 11:58:18.689689', '938', '10563'),
-  ('2021-06-17 11:58:18.689689', '938', '10564'),
-  ('2021-06-17 11:58:18.689689', '938', '10565'),
-  ('2021-06-17 11:58:18.689689', '938', '10566'),
-  ('2021-06-17 11:58:18.689689', '938', '10567'),
-  ('2021-06-17 11:58:18.689689', '938', '10568'),
-  ('2021-06-17 11:58:18.689689', '938', '10569'),
-  ('2021-06-17 11:58:18.689689', '938', '10570'),
-  ('2021-06-17 11:58:18.690690', '938', '10571')
+  ('2021-10-10 10:44:03.378378', '942', '10608'),
+  ('2021-10-10 10:44:03.378378', '942', '10573'),
+  ('2021-10-10 10:44:03.379379', '942', '10609'),
+  ('2021-10-10 10:44:03.379379', '942', '10610'),
+  ('2021-10-10 10:44:03.380380', '942', '10577'),
+  ('2021-10-10 10:44:03.380380', '942', '10574'),
+  ('2021-10-10 10:44:03.381381', '942', '10611'),
+  ('2021-10-10 10:44:03.381381', '942', '10572'),
+  ('2021-10-10 10:44:03.381381', '942', '10612'),
+  ('2021-10-10 10:44:03.381381', '942', '10581'),
+  ('2021-10-10 10:44:03.381381', '942', '10613'),
+  ('2021-10-10 10:44:03.381381', '942', '10614'),
+  ('2021-10-10 10:44:03.382382', '942', '10615'),
+  ('2021-10-10 10:44:03.382382', '942', '10616'),
+  ('2021-10-10 10:44:03.382382', '942', '10617'),
+  ('2021-10-10 10:44:03.382382', '942', '10618'),
+  ('2021-10-10 10:44:03.382382', '942', '10329'),
+  ('2021-10-10 10:44:03.382382', '942', '10619'),
+  ('2021-10-10 10:44:03.382382', '942', '10072'),
+  ('2021-10-10 10:44:03.383383', '942', '10620'),
+  ('2021-10-10 10:44:03.383383', '942', '10621')
   ;
 
   // Update to source_song table
