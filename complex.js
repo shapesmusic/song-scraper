3b747f760949d4ebe706b2afc3aa4183a7f34873
davidforrest <davidmforrest@gmail.com>
2021-10-13 19:09:14 -0400

    Update Complex for 2021-07-09

--- complex.js
+++ complex.js
@@ -47,7 +47,8 @@
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('Complex', 'Best New Music This Week', 'Brent Faiyaz, Drake, Nas, Rico Nasty, and More', '2021-07-01 12:00:00.000000', 'https://www.complex.com/music/best-new-music-this-week-brent-faiyaz-drake-nas-rico-nasty/');
+    ('Complex', 'Best New Music This Week', 'Vince Staples, Snoh Aalegra, Post Malone, and More', '2021-07-09 12:00:00.000000', 'https://www.complex.com/music/best-new-music-this-week-vince-staples-snoh-aalegra-post-malone/bia-nicki-minaj-whole-lotta-money-remix');
+
   // Update to source table
 
 
@@ -55,7 +56,7 @@
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 1036; // SELECT last_insert_rowid();
+  source_id = 1037; // SELECT last_insert_rowid();
   song_id = null;
 
   elements = document.getElementsByClassName("article-list");
@@ -100,49 +101,85 @@
   songsData =
   [
       {
-          "title": "Wasting Time",
-          "artist_name": "Brent Faiyaz ft. Drake",
+          "title": "Mhm",
+          "artist_name": "Vince Staples",
           "video_id": null,
-          "capture_date": "2021-10-13 03:23:08.766766",
-          "source_id": 1036,
-          "song_id": 10650,
+          "capture_date": "2021-10-13 07:04:34.611611",
+          "source_id": 1037,
+          "song_id": null,
+          "duplicate": false
+      },
+      {
+          "title": "Neon Peach",
+          "artist_name": "Snoh Aalegra ft. Tyler, the Creator",
+          "video_id": null,
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
+          "song_id": null,
+          "duplicate": false
+      },
+      {
+          "title": "RED",
+          "artist_name": "IDK ft. MF DOOM, Jay Electronica, & Westside Gunn",
+          "video_id": null,
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
+          "song_id": null,
+          "duplicate": false
+      },
+      {
+          "title": "Motley Crew",
+          "artist_name": "Post Malone",
+          "video_id": null,
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
+          "song_id": 10659,
           "duplicate": true
       },
       {
-          "title": "I Believed It",
-          "artist_name": "DVSN & Ty Dolla Sign ft. Mac Miller",
+          "title": "The Jackie",
+          "artist_name": "Bas ft. J. Cole & Lil Tjay",
           "video_id": null,
-          "capture_date": "2021-10-13 03:23:08.768768",
-          "source_id": 1036,
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
+          "song_id": 10663,
+          "duplicate": true
+      },
+      {
+          "title": "Bouncin",
+          "artist_name": "Tinashe",
+          "video_id": null,
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
           "song_id": null,
           "duplicate": false
       },
       {
-          "title": "Life Is Like a Dice Game",
-          "artist_name": "Nas ft. Freddie Gibbs & Cordae",
+          "title": "Stay",
+          "artist_name": "The Kid Laroi & Justin Bieber",
           "video_id": null,
-          "capture_date": "2021-10-13 03:23:08.768768",
-          "source_id": 1036,
-          "song_id": null,
-          "duplicate": false
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
+          "song_id": 10658,
+          "duplicate": true
       },
       {
-          "title": "Magic",
-          "artist_name": "Rico Nasty",
+          "title": "NDA",
+          "artist_name": "Billie Eilish",
           "video_id": null,
-          "capture_date": "2021-10-13 03:23:08.768768",
-          "source_id": 1036,
-          "song_id": null,
-          "duplicate": false
+          "capture_date": "2021-10-13 07:04:34.613613",
+          "source_id": 1037,
+          "song_id": 10661,
+          "duplicate": true
       },
       {
-          "title": "Love Music",
-          "artist_name": "Lil Yachty",
+          "title": "Whole Lotta Money (Remix)",
+          "artist_name": "BIA ft. Nicki Minaj",
           "video_id": null,
-          "capture_date": "2021-10-13 03:23:08.768768",
-          "source_id": 1036,
-          "song_id": null,
-          "duplicate": false
+          "capture_date": "2021-10-13 07:04:34.614614",
+          "source_id": 1037,
+          "song_id": 10660,
+          "duplicate": true
       }
   ]
 
@@ -183,10 +220,10 @@
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-  ('I Believed It', 'DVSN & Ty Dolla Sign ft. Mac Miller', NULL),
-  ('Life Is Like a Dice Game', 'Nas ft. Freddie Gibbs & Cordae', NULL),
-  ('Magic', 'Rico Nasty', NULL),
-  ('Love Music', 'Lil Yachty', NULL)
+  ('Mhm', 'Vince Staples', NULL),
+  ('Neon Peach', 'Snoh Aalegra ft. Tyler, the Creator', NULL),
+  ('RED', 'IDK ft. MF DOOM, Jay Electronica, & Westside Gunn', NULL),
+  ('Bouncin', 'Tinashe', NULL)
   ;
 
    // Update to song table
@@ -197,7 +234,7 @@
  //
 
   // Get the last song_id inserted
-  song_id = 11052; // SELECT last_insert_rowid();
+  song_id = 11056; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -237,11 +274,15 @@
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-  ('2021-10-13 03:23:08.766766', '1036', '10650'),
-  ('2021-10-13 03:23:08.768768', '1036', '11049'),
-  ('2021-10-13 03:23:08.768768', '1036', '11050'),
-  ('2021-10-13 03:23:08.768768', '1036', '11051'),
-  ('2021-10-13 03:23:08.768768', '1036', '11052')
+  ('2021-10-13 07:04:34.611611', '1037', '11053'),
+  ('2021-10-13 07:04:34.613613', '1037', '11054'),
+  ('2021-10-13 07:04:34.613613', '1037', '11055'),
+  ('2021-10-13 07:04:34.613613', '1037', '10659'),
+  ('2021-10-13 07:04:34.613613', '1037', '10663'),
+  ('2021-10-13 07:04:34.613613', '1037', '11056'),
+  ('2021-10-13 07:04:34.613613', '1037', '10658'),
+  ('2021-10-13 07:04:34.613613', '1037', '10661'),
+  ('2021-10-13 07:04:34.614614', '1037', '10660')
   ;
 
   // Update to source_song table
