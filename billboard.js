a9a6edcd548c708a1b6e2b43e8e7de404976a631
ll-studio <ll-studio@ll-studios-MacBook-Pro.local>
2021-12-30 09:50:15 -0500

    Update Billboard for 2021-11-20

--- billboard.js
+++ billboard.js
@@ -42,7 +42,7 @@
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('Billboard', 'The Hot 100', 'Week of November 13, 2021', '2021-11-13 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-11-13/');
+    ('Billboard', 'The Hot 100', 'Week of November 20, 2021', '2021-11-20 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-11-20/');
 
   // Update to source table
 
@@ -51,7 +51,7 @@
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 1054; // SELECT last_insert_rowid();
+  source_id = 1055; // SELECT last_insert_rowid();
   song_id = null;
 
   // elements = document.getElementsByClassName('chart-list__element display--flex');
@@ -103,47 +103,227 @@
   songsData =
   [
     {
-        "title": "Overpass Graffiti",
-        "artist_name": "Ed Sheeran",
+        "title": "One Right Now",
+        "artist_name": "Post Malone & The Weeknd",
         "video_id": null,
-        "capture_date": "2021-12-30 09:35:49.718718",
-        "source_id": 1054,
+        "capture_date": "2021-12-30 09:42:00.596596",
+        "source_id": 1055,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Demon High",
-        "artist_name": "Lil Uzi Vert",
+        "title": "Smokin Out The Window",
+        "artist_name": "Silk Sonic (Bruno Mars & Anderson .Paak)",
         "video_id": null,
-        "capture_date": "2021-12-30 09:35:49.719719",
-        "source_id": 1054,
+        "capture_date": "2021-12-30 09:42:00.596596",
+        "source_id": 1055,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Rap Freaks",
-        "artist_name": "Yung Miami",
+        "title": "Escape Plan",
+        "artist_name": "Travis Scott",
         "video_id": null,
-        "capture_date": "2021-12-30 09:35:49.719719",
-        "source_id": 1054,
+        "capture_date": "2021-12-30 09:42:00.596596",
+        "source_id": 1055,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Eat It",
-        "artist_name": "Megan Thee Stallion",
+        "title": "No Love",
+        "artist_name": "Summer Walker & SZA",
         "video_id": null,
-        "capture_date": "2021-12-30 09:35:49.719719",
-        "source_id": 1054,
+        "capture_date": "2021-12-30 09:42:00.597597",
+        "source_id": 1055,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Ghost Town",
-        "artist_name": "Benson Boone",
+        "title": "Bitter",
+        "artist_name": "Summer Walker & Cardi B",
         "video_id": null,
-        "capture_date": "2021-12-30 09:35:49.720720",
-        "source_id": 1054,
+        "capture_date": "2021-12-30 09:42:00.597597",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Mafia",
+        "artist_name": "Travis Scott",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.597597",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Throw It Away",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.598598",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Reciprocate",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.598598",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Toxic",
+        "artist_name": "Summer Walker Featuring Lil Durk",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.598598",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Unloyal",
+        "artist_name": "Summer Walker & Ari Lennox",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.598598",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Constant Bulls**t",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.598598",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "You Don't Know Me",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.598598",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Insane",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.599599",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Circus",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.599599",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "4th Baby Mama",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.599599",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Switch A N*gga Out",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.599599",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Closure",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.599599",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Session 33",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.599599",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Screwin",
+        "artist_name": "Summer Walker & Omarion",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Broken Promises",
+        "artist_name": "Summer Walker",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Dat Right There",
+        "artist_name": "Summer Walker, Pharrell Williams & The Neptunes",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Sad Girlz Luv Money",
+        "artist_name": "Amaarae & Moliy Featuring Kali Uchis",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
+        "song_id": 10963,
+        "duplicate": true
+    },
+    {
+        "title": "Slow Down Summer",
+        "artist_name": "Thomas Rhett",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Jumpin",
+        "artist_name": "NLE Choppa Featuring Polo G",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Super Gremlin",
+        "artist_name": "Kodak Black",
+        "video_id": null,
+        "capture_date": "2021-12-30 09:42:00.600600",
+        "source_id": 1055,
         "song_id": null,
         "duplicate": false
     }
@@ -187,11 +367,30 @@
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-  ('Overpass Graffiti', 'Ed Sheeran', NULL),
-  ('Demon High', 'Lil Uzi Vert', NULL),
-  ('Rap Freaks', 'Yung Miami', NULL),
-  ('Eat It', 'Megan Thee Stallion', NULL),
-  ('Ghost Town', 'Benson Boone', NULL)
+  ('One Right Now', 'Post Malone & The Weeknd', NULL),
+  ('Smokin Out The Window', 'Silk Sonic (Bruno Mars & Anderson .Paak)', NULL),
+  ('Escape Plan', 'Travis Scott', NULL),
+  ('No Love', 'Summer Walker & SZA', NULL),
+  ('Bitter', 'Summer Walker & Cardi B', NULL),
+  ('Mafia', 'Travis Scott', NULL),
+  ('Throw It Away', 'Summer Walker', NULL),
+  ('Reciprocate', 'Summer Walker', NULL),
+  ('Toxic', 'Summer Walker Featuring Lil Durk', NULL),
+  ('Unloyal', 'Summer Walker & Ari Lennox', NULL),
+  ('Constant Bulls**t', 'Summer Walker', NULL),
+  ('You Don’t Know Me', 'Summer Walker', NULL),
+  ('Insane', 'Summer Walker', NULL),
+  ('Circus', 'Summer Walker', NULL),
+  ('4th Baby Mama', 'Summer Walker', NULL),
+  ('Switch A N*gga Out', 'Summer Walker', NULL),
+  ('Closure', 'Summer Walker', NULL),
+  ('Session 33', 'Summer Walker', NULL),
+  ('Screwin', 'Summer Walker & Omarion', NULL),
+  ('Broken Promises', 'Summer Walker', NULL),
+  ('Dat Right There', 'Summer Walker, Pharrell Williams & The Neptunes', NULL),
+  ('Slow Down Summer', 'Thomas Rhett', NULL),
+  ('Jumpin', 'NLE Choppa Featuring Polo G', NULL),
+  ('Super Gremlin', 'Kodak Black', NULL)
   ;
 
    // Update to song table
@@ -202,7 +401,7 @@
  //
 
   // Get the last song_id inserted
-  song_id = 11166; // SELECT last_insert_rowid();
+  song_id = 11190; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -242,11 +441,31 @@
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-  ('2021-12-30 09:35:49.718718', '1054', '11162'),
-  ('2021-12-30 09:35:49.719719', '1054', '11163'),
-  ('2021-12-30 09:35:49.719719', '1054', '11164'),
-  ('2021-12-30 09:35:49.719719', '1054', '11165'),
-  ('2021-12-30 09:35:49.720720', '1054', '11166')
+  ('2021-12-30 09:42:00.596596', '1055', '11167'),
+  ('2021-12-30 09:42:00.596596', '1055', '11168'),
+  ('2021-12-30 09:42:00.596596', '1055', '11169'),
+  ('2021-12-30 09:42:00.597597', '1055', '11170'),
+  ('2021-12-30 09:42:00.597597', '1055', '11171'),
+  ('2021-12-30 09:42:00.597597', '1055', '11172'),
+  ('2021-12-30 09:42:00.598598', '1055', '11173'),
+  ('2021-12-30 09:42:00.598598', '1055', '11174'),
+  ('2021-12-30 09:42:00.598598', '1055', '11175'),
+  ('2021-12-30 09:42:00.598598', '1055', '11176'),
+  ('2021-12-30 09:42:00.598598', '1055', '11177'),
+  ('2021-12-30 09:42:00.598598', '1055', '11178'),
+  ('2021-12-30 09:42:00.599599', '1055', '11179'),
+  ('2021-12-30 09:42:00.599599', '1055', '11180'),
+  ('2021-12-30 09:42:00.599599', '1055', '11181'),
+  ('2021-12-30 09:42:00.599599', '1055', '11182'),
+  ('2021-12-30 09:42:00.599599', '1055', '11183'),
+  ('2021-12-30 09:42:00.599599', '1055', '11184'),
+  ('2021-12-30 09:42:00.600600', '1055', '11185'),
+  ('2021-12-30 09:42:00.600600', '1055', '11186'),
+  ('2021-12-30 09:42:00.600600', '1055', '11187'),
+  ('2021-12-30 09:42:00.600600', '1055', '10963'),
+  ('2021-12-30 09:42:00.600600', '1055', '11188'),
+  ('2021-12-30 09:42:00.600600', '1055', '11189'),
+  ('2021-12-30 09:42:00.600600', '1055', '11190')
   ;
 
   // Update to source_song table
