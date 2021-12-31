763cf8aa6d4e4cbbf093fc4c2a952a5b1c294e44
ll-studio <ll-studio@ll-studios-MacBook-Pro.local>
2021-12-30 19:05:24 -0500

    Update Billboard for 2021-12-25

--- billboard.js
+++ billboard.js
@@ -42,7 +42,7 @@
   INSERT INTO source
     (parent_entity, parent_stream, instance_name, publication_date, location)
   VALUES
-    ('Billboard', 'The Hot 100', 'Week of December 18, 2021', '2021-12-18 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-12-18/');
+    ('Billboard', 'The Hot 100', 'Week of December 25, 2021', '2021-12-25 12:00:00.000000', 'https://www.billboard.com/charts/hot-100/2021-12-25/');
 
   // Update to source table
 
@@ -51,7 +51,7 @@
 // Step 2: Scrape song data into an array
 //
 
-  source_id = 1059; // SELECT last_insert_rowid();
+  source_id = 1060; // SELECT last_insert_rowid();
   song_id = null;
 
   // elements = document.getElementsByClassName('chart-list__element display--flex');
@@ -103,74 +103,110 @@
   songsData =
   [
     {
-        "title": "I Hate U",
-        "artist_name": "SZA",
+        "title": "Girl Of My Dreams",
+        "artist_name": "Juice WRLD & SUGA",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.953953",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.811811",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Wandered To LA.",
-        "artist_name": "Juice WRLD & Justin Bieber",
+        "title": "Burn",
+        "artist_name": "Juice WRLD",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.954954",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.811811",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Don't Play",
-        "artist_name": "Polo G Featuring Lil Baby",
+        "title": "Feline",
+        "artist_name": "Juice WRLD, Polo G & Trippie Redd",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.955955",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.812812",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Me Or Sum",
-        "artist_name": "Nardo Wick, Lil Baby & Future",
+        "title": "Rockstar In His Prime",
+        "artist_name": "Juice WRLD",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.956956",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.812812",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Young N Dumb",
-        "artist_name": "Polo G",
+        "title": "You Wouldn't Understand",
+        "artist_name": "Juice WRLD",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.956956",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.812812",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Start Up Again",
-        "artist_name": "Polo G Featuring Moneybagg Yo",
+        "title": "Doom",
+        "artist_name": "Juice WRLD",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.956956",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.812812",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Mercedes",
-        "artist_name": "Brent Faiyaz",
+        "title": "Go Hard",
+        "artist_name": "Juice WRLD",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.956956",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.812812",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     },
     {
-        "title": "Partin Ways",
-        "artist_name": "Polo G",
+        "title": "Not Enough",
+        "artist_name": "Juice WRLD",
         "video_id": null,
-        "capture_date": "2021-12-30 06:53:35.956956",
-        "source_id": 1059,
+        "capture_date": "2021-12-30 07:00:34.812812",
+        "source_id": 1060,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Man In The Mirror",
+        "artist_name": "A Boogie Wit da Hoodie",
+        "video_id": null,
+        "capture_date": "2021-12-30 07:00:34.813813",
+        "source_id": 1060,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "From My Window",
+        "artist_name": "Juice WRLD",
+        "video_id": null,
+        "capture_date": "2021-12-30 07:00:34.813813",
+        "source_id": 1060,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Relocate",
+        "artist_name": "Juice WRLD",
+        "video_id": null,
+        "capture_date": "2021-12-30 07:00:34.813813",
+        "source_id": 1060,
+        "song_id": null,
+        "duplicate": false
+    },
+    {
+        "title": "Feel Alone",
+        "artist_name": "Juice WRLD",
+        "video_id": null,
+        "capture_date": "2021-12-30 07:00:34.813813",
+        "source_id": 1060,
         "song_id": null,
         "duplicate": false
     }
@@ -213,14 +249,18 @@
   INSERT INTO song
     (title, artist_name, video_id)
   VALUES
-  ('I Hate U', 'SZA', NULL),
-  ('Wandered To LA.', 'Juice WRLD & Justin Bieber', NULL),
-  ('Don’t Play', 'Polo G Featuring Lil Baby', NULL),
-  ('Me Or Sum', 'Nardo Wick, Lil Baby & Future', NULL),
-  ('Young N Dumb', 'Polo G', NULL),
-  ('Start Up Again', 'Polo G Featuring Moneybagg Yo', NULL),
-  ('Mercedes', 'Brent Faiyaz', NULL),
-  ('Partin Ways', 'Polo G', NULL)
+  ('Girl Of My Dreams', 'Juice WRLD & SUGA', NULL),
+  ('Burn', 'Juice WRLD', NULL),
+  ('Feline', 'Juice WRLD, Polo G & Trippie Redd', NULL),
+  ('Rockstar In His Prime', 'Juice WRLD', NULL),
+  ('You Wouldn’t Understand', 'Juice WRLD', NULL),
+  ('Doom', 'Juice WRLD', NULL),
+  ('Go Hard', 'Juice WRLD', NULL),
+  ('Not Enough', 'Juice WRLD', NULL),
+  ('Man In The Mirror', 'A Boogie Wit da Hoodie', NULL),
+  ('From My Window', 'Juice WRLD', NULL),
+  ('Relocate', 'Juice WRLD', NULL),
+  ('Feel Alone', 'Juice WRLD', NULL)
   ;
 
    // Update to song table
@@ -231,7 +271,7 @@
  //
 
   // Get the last song_id inserted
-  song_id = 11249; // SELECT last_insert_rowid();
+  song_id = 11261; // SELECT last_insert_rowid();
 
   // Calculate the number of nonduplicate songs added
   nonduplicates = 0;
@@ -271,14 +311,18 @@
   INSERT INTO source_song
     (capture_date, source_id, song_id)
   VALUES
-  ('2021-12-30 06:53:35.953953', '1059', '11242'),
-  ('2021-12-30 06:53:35.954954', '1059', '11243'),
-  ('2021-12-30 06:53:35.955955', '1059', '11244'),
-  ('2021-12-30 06:53:35.956956', '1059', '11245'),
-  ('2021-12-30 06:53:35.956956', '1059', '11246'),
-  ('2021-12-30 06:53:35.956956', '1059', '11247'),
-  ('2021-12-30 06:53:35.956956', '1059', '11248'),
-  ('2021-12-30 06:53:35.956956', '1059', '11249')
+  ('2021-12-30 07:00:34.811811', '1060', '11250'),
+  ('2021-12-30 07:00:34.811811', '1060', '11251'),
+  ('2021-12-30 07:00:34.812812', '1060', '11252'),
+  ('2021-12-30 07:00:34.812812', '1060', '11253'),
+  ('2021-12-30 07:00:34.812812', '1060', '11254'),
+  ('2021-12-30 07:00:34.812812', '1060', '11255'),
+  ('2021-12-30 07:00:34.812812', '1060', '11256'),
+  ('2021-12-30 07:00:34.812812', '1060', '11257'),
+  ('2021-12-30 07:00:34.813813', '1060', '11258'),
+  ('2021-12-30 07:00:34.813813', '1060', '11259'),
+  ('2021-12-30 07:00:34.813813', '1060', '11260'),
+  ('2021-12-30 07:00:34.813813', '1060', '11261')
   ;
 
   // Update to source_song table
